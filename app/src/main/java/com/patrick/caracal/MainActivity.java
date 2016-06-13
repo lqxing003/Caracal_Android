ff906f820b1a17dc3c932ecc136f61cbd6d389b4
Patrick <316463304@qq.com>
2016-06-13 14:09:28 +0800

    增加二维码扫码功能

--- app/src/main/java/com/patrick/caracal/MainActivity.java
+++ app/src/main/java/com/patrick/caracal/MainActivity.java
@@ -1,13 +1,37 @@
 package com.patrick.caracal;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.zxing.activity.CaptureActivity;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final int REQUEST_CODE = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CaptureActivity.class);
+                startActivityForResult(intent, REQUEST_CODE);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK){ //RESULT_OK = -1
+            Bundle bundle = data.getExtras();
+            String scanResult = bundle.getString("result");
+            Toast.makeText(MainActivity.this, scanResult, Toast.LENGTH_LONG).show();
+        }
     }
 }
