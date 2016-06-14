42bbed2833c775adc7d84501f35e003c455902a8
Patrick <316463304@qq.com>
2016-06-14 18:23:03 +0800

    更新bottom bar 颜色

--- app/src/main/java/com/patrick/caracal/MainActivity.java
+++ app/src/main/java/com/patrick/caracal/MainActivity.java
@@ -54,10 +54,10 @@ public class MainActivity extends AppCompatActivity {
 
         // Setting colors for different tabs when there's more than three of them.
         // You can set colors for tabs in three different ways as shown below.
-        bottomBar.mapColorForTab(0, ContextCompat.getColor(this, R.color.colorPrimary));
-        bottomBar.mapColorForTab(1, ContextCompat.getColor(this, R.color.colorPrimary));
-        bottomBar.mapColorForTab(2, ContextCompat.getColor(this, R.color.colorPrimary));
-        bottomBar.mapColorForTab(3, ContextCompat.getColor(this, R.color.colorPrimary));
+        bottomBar.mapColorForTab(0, ContextCompat.getColor(this, R.color.bottombar_color));
+        bottomBar.mapColorForTab(1, ContextCompat.getColor(this, R.color.bottombar_color));
+        bottomBar.mapColorForTab(2, ContextCompat.getColor(this, R.color.bottombar_color));
+        bottomBar.mapColorForTab(3, ContextCompat.getColor(this, R.color.bottombar_color));
 
 
         fragments.add(HomeFragment.newInstance());
